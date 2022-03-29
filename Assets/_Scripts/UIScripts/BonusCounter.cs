8bb86e4128633e854fcd361e0294c0dc90c94c2a
RaPloky <raplokygamingone@gmail.com>
2022-03-29 18:16:19 +0300

    Fixed destroy bonus and added counters for every bonus

---
+++ Assets/_Scripts/UIScripts/BonusCounter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BonusCounter : MonoBehaviour
+{
+    [SerializeField] private BonusManager bonus;
+    private TextMeshProUGUI countOfBonuses;
+
+    private void Awake()
+    {
+        countOfBonuses = GetComponent<TextMeshProUGUI>();
+    }
+    private void Update()
+    {
+        countOfBonuses.text = bonus.GetComponent<BonusManager>().bonusCounter.ToString();
+    }
+}
