af189f228ae0acb6c5dd3e54d231de4538ac98bd
RaPloky <raplokygamingone@gmail.com>
2022-03-29 18:57:41 +0300

    Added new planet models + added planet rotation script

---
+++ Assets/_Scripts/AnimationScripts/RotatePlanet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlanet : MonoBehaviour
+{
+    private Transform planetTransform;
+    [SerializeField] private Vector3 rotateDirection;
+    [SerializeField] private bool randomize;
+    [SerializeField] private float randomizeRange;
+    void Awake()
+    {
+        planetTransform = GetComponent<Transform>();
+        if (randomize)
+        {
+            rotateDirection.x = Random.Range(-randomizeRange, randomizeRange);
+            rotateDirection.y = Random.Range(-randomizeRange, randomizeRange);
+            rotateDirection.z = Random.Range(-randomizeRange, randomizeRange);
+        }
+    }
+    void FixedUpdate()
+    {
+        planetTransform.Rotate(rotateDirection);
+    }
+}
