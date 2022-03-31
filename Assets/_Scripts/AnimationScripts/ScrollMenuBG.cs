8db2e9fa0739a5e0e7a21c88545272b235d635d2
RaPloky <raplokygamingone@gmail.com>
2022-03-31 14:19:03 +0300

    Created scrolling menu BG

---
+++ Assets/_Scripts/AnimationScripts/ScrollMenuBG.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollMenuBG : MonoBehaviour
+{
+    [SerializeField] private RectTransform BG;
+    [SerializeField] private float scrollSpeedX;
+    [SerializeField] private float scrollLimitX;
+    private float scrollStartPosX;
+
+    private void Awake()
+    {
+        // Start position is x-position at first frame:
+        scrollStartPosX = BG.anchoredPosition.x;
+    }
+
+    private void Update()
+    {
+        BG.anchoredPosition = new Vector2(BG.anchoredPosition.x + scrollSpeedX, BG.anchoredPosition.y);
+
+        if (BG.anchoredPosition.x <= scrollLimitX)
+        {
+            BG.anchoredPosition = new Vector2(scrollStartPosX, BG.anchoredPosition.y);
+        }
+    }
+}
