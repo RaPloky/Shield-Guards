43c1f15939008d39ad1a23433c77f61da8e52848
RaPloky <raplokygamingone@gmail.com>
2022-06-14 17:43:42 +0300

    UI huge improvement
    Removed energy levels display. Instead of that, energy level is shown now by transparency of the button - the more energy - the more solid button is, and conversely.

---
+++ Assets/_Scripts/UIScripts/ChangeTransparancyBonus.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeTransparancyBonus : MonoBehaviour
+{
+    [SerializeField] private GameplayManager satellite;
+    private Image _buttonImage;
+
+    private void Awake()
+    {
+        _buttonImage = GetComponent<Image>();
+    }
+    private void Update()
+    {
+        if (satellite.isDicharged)
+        {
+            gameObject.SetActive(false);
+        }
+        float alphaValue = satellite.currentEnergyLevel / 100f;
+        _buttonImage.color = new Color(_buttonImage.color.r, _buttonImage.color.g, _buttonImage.color.b, alphaValue);
+    }
+
+}
