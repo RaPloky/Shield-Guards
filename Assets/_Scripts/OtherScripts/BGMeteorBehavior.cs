c979305a72974e4eafa921e834de36c523cd8737
RaPloky <raplokygamingone@gmail.com>
2022-06-22 19:06:38 +0300

    Improved meteor trail; added BG meteors

---
+++ Assets/_Scripts/OtherScripts/BGMeteorBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMeteorBehavior : MonoBehaviour
+{
+    [SerializeField] float speed;
+    private BGMeteorSpawner _spawner;
+    private Rigidbody _rb;
+    private Transform _target;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _spawner = gameObject.transform.parent.GetComponent<BGMeteorSpawner>();
+        _target = _spawner.target.transform;
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.AddForce((_target.position - _rb.position) * speed, ForceMode.Force);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/_Scripts/OtherScripts/BGMeteorSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMeteorSpawner : MonoBehaviour
+{
+    [Header("MeteorTarget")]
+    public GameObject target;
+    [Header("MeteorSpawn")]
+    [SerializeField] GameObject backgroundMeteor;
+    [SerializeField] [Range(3f, 10f)] float spawnDelay;
+    [SerializeField] [Range(1f, 5f)] float delayDispresion;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnBGMeteor());    
+    }
+
+    private IEnumerator SpawnBGMeteor()
+    {
+        float delayDispersion = Random.Range(0f, delayDispresion);
+        yield return new WaitForSeconds(spawnDelay + delayDispersion);
+        Instantiate(backgroundMeteor, gameObject.transform);
+        StartCoroutine(SpawnBGMeteor());
+    }
+}
