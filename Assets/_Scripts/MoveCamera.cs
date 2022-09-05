2eb50446dc32a1091f3cfa9a98c08b462915985b
RaPloky <raplokygamingone@gmail.com>
2022-09-05 13:40:15 +0300

    Added camera control; improved prefabs

---
+++ Assets/_Scripts/MoveCamera.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    [SerializeField, Range(0f, 1f)] float smoothFactor;
+    [SerializeField] Transform targetToFollow;
+    [SerializeField] Transform rotateAroundJoint;
+    [SerializeField] float angleChange;
+
+    private void FixedUpdate()
+    {
+        float posX = Mathf.Lerp(transform.position.x, targetToFollow.position.x, smoothFactor);
+        float posZ = Mathf.Lerp(transform.position.z, targetToFollow.position.z, smoothFactor);
+        transform.position = new Vector3(posX, transform.position.y, posZ);
+    }
+
+    public void TurnLeft()
+    {
+        targetToFollow.RotateAround(rotateAroundJoint.position, Vector3.up, angleChange);
+        transform.Rotate(angleChange * Vector3.up);
+    }
+    public void TurnRight()
+    {
+        targetToFollow.RotateAround(rotateAroundJoint.position, Vector3.down, angleChange);
+        transform.Rotate(angleChange * Vector3.down);
+    }
+}
