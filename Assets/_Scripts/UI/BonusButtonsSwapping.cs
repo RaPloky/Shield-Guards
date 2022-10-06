551bb4f89202dc4b77d2ba04e98b61535fc1caec
RaPloky <raplokygamingone@gmail.com>
2022-10-06 22:18:27 +0300

    Template for buttons swap ready

--- Assets/_Scripts/UI/BonusButtonsSwapping.cs
+++ Assets/_Scripts/UI/BonusButtonsSwapping.cs
@@ -4,5 +4,36 @@ using UnityEngine;
 
 public class BonusButtonsSwapping : MonoBehaviour
 {
+    [SerializeField] private Transform cameraTrans;
+    [SerializeField] private List<Transform> guards;
+    [SerializeField] private List<GameObject> bonusButtons;
 
+    private Transform _closestGuard;
+
+    private void Start()
+    {
+        ArrangeBonusButtons();
+    }
+
+    private void ArrangeBonusButtons()
+    {
+        int closestTargetIndex = 0;
+        float distanceToClosestGuard = float.MaxValue;
+
+        if (guards == null)
+            return;
+
+        for (int i = 0; i < guards.Count; i++)
+        {
+            Transform guardTrans = guards[i];
+            float distanceBetweenCameraAndGuard = Vector3.Distance(transform.position, guardTrans.position);
+
+            if (distanceBetweenCameraAndGuard < distanceToClosestGuard)
+            {
+                distanceToClosestGuard = distanceBetweenCameraAndGuard;
+                closestTargetIndex = i;
+            }
+        }
+        _closestGuard = guards[closestTargetIndex];
+    }
 }
