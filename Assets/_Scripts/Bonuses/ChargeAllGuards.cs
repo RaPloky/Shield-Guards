9cf84132936142f1b85f6f3815efd397c96f76d2
RaPloky <raplokygamingone@gmail.com>
2022-10-12 23:44:52 +0300

    + charge bonus

--- Assets/_Scripts/Bonuses/ChargeAllGuards.cs
+++ Assets/_Scripts/Bonuses/ChargeAllGuards.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class ChargeAllGuards : MonoBehaviour
 {
+    [SerializeField] private List<Guard> guards;
+    [SerializeField] private int chargeAmount;
+    [SerializeField] private bool isBonusEnabled;
 
+    public void ActivateInstantCharging()
+    {
+        if (!isBonusEnabled)
+            return;
+
+        foreach (Guard guard in guards)
+        {
+            guard.AddEnergy(chargeAmount);
+        }
+        isBonusEnabled = false;
+    }
 }
