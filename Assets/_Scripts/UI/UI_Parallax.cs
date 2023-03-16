9b7ca6d337c9c8d2a78ea0c623757deeba004db3
RaPloky <raplokygamingone@gmail.com>
2023-03-16 06:45:14 +0200

    + parallax and menu update

---
+++ Assets/_Scripts/UI/UI_Parallax.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Parallax : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Transform _startPoint;
+    [SerializeField] private Transform _endPoint;
+
+    private Transform _thatPart;
+
+    private void Start()
+    {
+        _thatPart = GetComponent<Transform>();
+    }
+
+    private void FixedUpdate()
+    {
+        _thatPart.position = new Vector3(_thatPart.position.x + _speed * Time.deltaTime, _thatPart.position.y, _thatPart.position.z);
+
+        if (_thatPart.position.x >= _endPoint.position.x)
+            ResetPosition();
+    }
+
+    private void ResetPosition()
+    {
+        _thatPart.position = _startPoint.position;
+    }
+}
