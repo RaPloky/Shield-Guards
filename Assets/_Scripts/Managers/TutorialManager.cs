3e53c01f823752549a73fa8abf3177306a008ddc
RaPloky <raplokygamingone@gmail.com>
2023-09-07 11:59:03 +0300

    + template for tutorial scene

--- Assets/_Scripts/Managers/TutorialManager.cs
+++ Assets/_Scripts/Managers/TutorialManager.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
+    [SerializeField] private GameObject[] popUps;
 
+    private int _popIndex;
+
+    private void Update()
+    {
+        for (int i = 0; i < popUps.Length; i++)
+        {
+            if (i == _popIndex)
+            {
+                popUps[_popIndex].SetActive(true);
+            }
+            else
+            {
+                popUps[i].SetActive(false);
+            }
+        }
+
+        if (_popIndex == 0)
+        {
+            // if instruction done, then _popIndex++
+        }
+    }
 }
